fc29846ba0a1248ead94abe08653ccec1169c5f9
Sean <sean@awoo.com.tw>
2018-04-20 09:10:44 +0800

    update

---
+++ index.php
@@ -0,0 +1,117 @@
+<?php include('top.php'); ?>
+<h3 style="color: #0000FF;">jp_guam_front</h3>
+<table>
+	<thead>
+		<tr>
+			<th width="120">項目</th><th width="170">&nbsp;</th><th>&nbsp;</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>開發機 - 網址</td>
+			<td><a href="http://localhost/guam/jp_guam_front/" target="_blank">前台</a> <a href="http://localhost/guam/jp_guam_front/guam_staff" target="_blank">後台</a></td>
+			<td>&nbsp;</td>
+		</tr>
+		<tr>
+			<td>測式機 - 網址</td>
+			<td><a href="https://test9-seanjp.guam.com.tw/" target="_blank">前台</a> <a href="https://test9-seanjp.guam.com.tw/guam_staff" target="_blank">後台</a> <a href="https://car-guam.hostgoods.com/" target="_blank">外包</a></td>
+			<td>&nbsp;</td>
+		</tr>
+		<tr>
+			<td>測式機 - 資料庫</td>
+			<td><a href="https://testam9.guam.com.tw/" target="_blank">這裡</a></td>
+			<td>ddl_rd_sean, SG4EdYBQMIKqwUzv</td>
+		</tr>
+		<tr>
+			<td>企劃</td>
+			<td><a href="https://docs.google.com/spreadsheets/d/14lYTQdoj3cvejK0A266BPbSWu7Ok-puwOkmMopCotCM/edit?ts=5ac1e177#gid=0" target="_blank">時程</a> <a href="https://drive.google.com/drive/u/0/folders/1h70b-MWVx3c8KvSkiIuJZqcwjoA67P64" target="_blank">開飛機</a></td>
+			<td></td>
+		</tr>
+	</tbody>
+</table>
+<h3 style="color: #0000FF;">stagedictionaryapp</h3>
+<table>
+	<thead>
+		<tr>
+			<th width="120">項目</th><th width="170">&nbsp;</th><th>&nbsp;</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>開發機 - 網址</td>
+			<td><a href="http://stagedictionaryapp.awoo.org/tkserv/sean" target="_blank">這裡</a></td>
+			<td>&nbsp;</td>
+		</tr>
+	</tbody>
+</table>
+<h3 style="color: #0000FF;">guam_topic</h3>
+<table>
+	<thead>
+		<tr>
+			<th width="120">項目</th><th width="170">&nbsp;</th><th>&nbsp;</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>開發機 - 網址</td>
+			<td><a href="http://devwedding.guam.com.tw/" target="_blank">這裡</a></td>
+			<td>&nbsp;</td>
+		</tr>
+		<tr>
+			<td>測式機 - 網址</td>
+			<td><a href="https://beta9-wedding.guam.com.tw/" target="_blank">這裡</a></td>
+			<td>&nbsp;</td>
+		</tr>
+		<tr>
+			<td>正式機 - 網址</td>
+			<td><a href="https://wedding.guam.com.tw/" target="_blank">這裡</a></td>
+			<td>&nbsp;</td>
+		</tr>
+		<tr>
+			<td>測式機 - 資料庫</td>
+			<td><a href="https://testam9.guam.com.tw/" target="_blank">這裡</a></td>
+			<td>ddl_rd_sean, SG4EdYBQMIKqwUzv</td>
+		</tr>
+		<tr>
+			<td>正式機 - 資料庫</td>
+			<td><a href="https://am9.guam.com.tw/" target="_blank">這裡</a></td>
+			<td>ddl_rd_sean, Nus4AcFNiw27swc6</td>
+		</tr>
+	</tbody>
+</table>
+<h3 style="color: #0000FF;">guam</h3>
+<table>
+	<thead>
+		<tr>
+			<th width="120">項目</th><th width="170">&nbsp;</th><th>&nbsp;</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>開發機 - 網址</td>
+			<td><a href="http://localhost/guam/guam/" target="_blank">前台</a> <a href="http://localhost/guam/guam/guam_staff/user/login" target="_blank">後台</a></td>
+			<td>&nbsp;</td>
+		</tr>
+		<tr>
+			<td>測式機 - 網址</td>
+			<td><a href="https://beta9.guam.com.tw/" target="_blank">前台</a> <a href="https://beta9.guam.com.tw/guam_staff/user/login" target="_blank">後台</a></td>
+			<td>&nbsp;</td>
+		</tr>
+		<tr>
+			<td>正式機 - 網址</td>
+			<td><a href="https://www.guam.com.tw/" target="_blank">前台</a> <a href="https://www.guam.com.tw/guam_staff/user/login" target="_blank">後台</a></td>
+			<td>&nbsp;</td>
+		</tr>
+		<tr>
+			<td>測式機 - 資料庫</td>
+			<td><a href="https://testam9.guam.com.tw/" target="_blank">這裡</a></td>
+			<td>ddl_rd_sean, SG4EdYBQMIKqwUzv</td>
+		</tr>
+		<tr>
+			<td>正式機 - 資料庫</td>
+			<td><a target="_bland" href="https://am9.guam.com.tw/">這裡</a></td>
+			<td>ddl_rd_sean, Nus4AcFNiw27swc6</td>
+		</tr>
+	</tbody>
+</table>
+<?php include('bottom.php'); ?>
